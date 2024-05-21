05d844b7891c2bbab422eabf218403515176b8a5
ahargunyllib <billy.bpm03@gmail.com>
2024-05-21 15:35:03 +0700

    style: add default background color

--- app/src/main/java/javamon/frontend/Panel.java
+++ app/src/main/java/javamon/frontend/Panel.java
@@ -3,12 +3,15 @@ package javamon.frontend;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import javamon.frontend.styles.Colors;
+
 public abstract class Panel extends JPanel {
     protected final HomeGUI homeGUI;
 
     public Panel(HomeGUI homeGUI) {
         this.homeGUI = homeGUI;
         setBorder(new EmptyBorder(16, 16, 16, 16));
+        setBackground(Colors.BACKGROUND);
     }
 
     public abstract void refresh();
