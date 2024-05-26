c72a2befb54dc78ec01dcf887a564b6f99094eef
ahargunyllib <billy.bpm03@gmail.com>
2024-05-26 22:53:03 +0700

    refactor: fixing

--- app/src/main/java/javamon/frontend/homebase/ShopPanel.java
+++ app/src/main/java/javamon/frontend/homebase/ShopPanel.java
@@ -28,19 +28,14 @@ public class ShopPanel extends Panel {
 
         Row header = new Row();
 
-        Button backBtn = new Button("Back", "Inter-Bold", 16f, Color.WHITE, Color.BLACK, back(homeGUI));
-        Label pageLbl = new Label("Shop", "jua", 32f, 8, Color.WHITE, Color.BLACK);
-        Label goldLbl = new Label(Javamon.getPLAYER().getGold() + " gold", "jua", 16f, 8, Color.WHITE, Color.BLACK);
+        Button backBtn = new Button("Back", "Inter-Bold", 16f, Colors.SHOP_ACCENT, Color.BLACK, back(homeGUI));
+        Label pageLbl = new Label("Shop", "jua", 32f, 8, Colors.SHOP_ACCENT, Color.BLACK);
+        Label goldLbl = new Label(Javamon.getPLAYER().getGold() + " gold", "jua", 16f, 8, Colors.SHOP_ACCENT,
+                Color.BLACK);
 
         header.add(backBtn);
         header.add(Box.createHorizontalGlue());
-        header.add(pageLbl);
-        header.add(Box.createHorizontalGlue());
         header.add(goldLbl);
-
-        add(header);
-
-        add(SizedBox.height(32));
 
         Map<Item, Integer> itemToBuy = new HashMap<>();
         for (Item item : Javamon.getITEMS()) {
@@ -51,20 +46,43 @@ public class ShopPanel extends Panel {
         for (Item item : itemToBuy.keySet()) {
             total += item.getPrice() * itemToBuy.get(item);
         }
-        Label totalLbl = new Label("Total: " + total + " gold", "jua", 16f, 8, Color.WHITE, Color.BLACK);
+        Label totalLbl = new Label("Total: " + total + " gold", "jua", 16f, 8, Colors.SHOP_ACCENT, Color.BLACK);
 
         Row itemPanel = new Row();
 
         Column healPanel = getHealItems(itemToBuy, totalLbl);
+        Column elementalPanel = getElementalItems(itemToBuy, totalLbl);
 
+        itemPanel.add(Box.createHorizontalGlue());
+        itemPanel.add(healPanel);
+        itemPanel.add(SizedBox.width(32));
+        itemPanel.add(elementalPanel);
+        itemPanel.add(Box.createHorizontalGlue());
+
+        Button buyBtn = getBuyBtn(homeGUI, itemToBuy);
+
+        add(SizedBox.height(8));
+        add(header);
+        add(SizedBox.height(16));
+        add(pageLbl);
+        add(SizedBox.height(8));
+        add(itemPanel);
+        add(SizedBox.height(16));
+        add(totalLbl);
+        add(SizedBox.height(8));
+        add(buyBtn);
+        add(Box.createVerticalGlue());
+    }
+
+    private Column getElementalItems(Map<Item, Integer> itemToBuy, Label totalLbl) {
         Column elementalPanel = new Column();
         elementalPanel.setBackground(Colors.Elemental);
 
-        Label elementalLbl = new Label("Elemental Items", "jua", 16f, 8, Color.WHITE, Color.BLACK);
-        
+        Label elementalLbl = new Label("Elemental Items", "jua", 16f, 8, Colors.SHOP_ACCENT, Color.BLACK);
+
         ImageIcon elementalItemIcon = new ImageIcon("assets/images/items/elemental.png");
-        int newWidth = 256;
-        int newHeight = 256;
+        int newWidth = 128;
+        int newHeight = 128;
         Image img = elementalItemIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
         ImageIcon elementalItemIconScaled = new ImageIcon(img);
         JLabel elementalItemIconLbl = new JLabel(elementalItemIconScaled);
@@ -72,56 +90,55 @@ public class ShopPanel extends Panel {
 
         elementalPanel.add(SizedBox.height(32));
         elementalPanel.add(elementalLbl);
-        elementalPanel.add(SizedBox.height(64));
+        elementalPanel.add(SizedBox.height(32));
         elementalPanel.add(elementalItemIconLbl);
-        elementalPanel.add(SizedBox.height(64));
+        elementalPanel.add(SizedBox.height(32));
 
         Row[] elementalItems = new Row[3];
         for (int i = 3; i < 6; i++) {
             elementalItems[i - 3] = new Row();
             Label elementalItemLbl = new Label(
                     Javamon.getItem(i).getName() + " - " + Javamon.getItem(i).getPrice() + " gold",
-                    "jua", 16f, 8, Color.WHITE, Color.BLACK);
-            Button addItem = new Button("+", "Inter-Bold", 16f, Color.WHITE, Color.BLACK,
+                    "jua", 16f, 8, Colors.SHOP_ACCENT, Color.BLACK);
+            Button addItem = new Button("+", "Inter-Bold", 16f, Colors.SHOP_ACCENT, Color.BLACK,
                     addItem(itemToBuy, i, totalLbl));
-            Button removeItem = new Button("-", "Inter-Bold", 16f, Color.WHITE, Color.BLACK,
+            Button removeItem = new Button("-", "Inter-Bold", 16f, Colors.SHOP_ACCENT, Color.BLACK,
                     removeItem(itemToBuy, i, totalLbl));
+            addItem.setSize(48, 48);
+            addItem.setPreferredSize(new Dimension(48, 48));
+            addItem.setMaximumSize(new Dimension(48, 48));
+            addItem.setMinimumSize(new Dimension(48, 48));
 
-            elementalItems[i - 3].add(Box.createHorizontalGlue());
+            removeItem.setSize(48, 48);
+            removeItem.setPreferredSize(new Dimension(48, 48));
+            removeItem.setMaximumSize(new Dimension(48, 48));
+            removeItem.setMinimumSize(new Dimension(48, 48));
+
+            elementalItems[i - 3].add(SizedBox.width(8));
             elementalItems[i - 3].add(elementalItemLbl);
             elementalItems[i - 3].add(SizedBox.width(8));
             elementalItems[i - 3].add(addItem);
             elementalItems[i - 3].add(SizedBox.width(8));
             elementalItems[i - 3].add(removeItem);
-            elementalItems[i - 3].add(Box.createHorizontalGlue());
+            elementalItems[i - 3].add(SizedBox.width(8));
 
             elementalPanel.add(elementalItems[i - 3]);
         }
-        
+
         elementalPanel.add(SizedBox.height(32));
 
-        itemPanel.add(healPanel);
-        itemPanel.add(Box.createHorizontalStrut(8));
-        itemPanel.add(elementalPanel);
-
-        add(itemPanel);
-        add(SizedBox.height(32));
-        add(totalLbl);
-        add(SizedBox.height(8));
-
-        Button buyBtn = getBuyBtn(homeGUI, itemToBuy);
-        add(buyBtn);
+        return elementalPanel;
     }
 
     private Column getHealItems(Map<Item, Integer> itemToBuy, Label totalLbl) {
         Column healPanel = new Column();
         healPanel.setBackground(Colors.HP);
 
-        Label healLbl = new Label("Healing Items", "jua", 16f, 8, Color.WHITE, Color.BLACK);
+        Label healLbl = new Label("Healing Items", "jua", 16f, 8, Colors.SHOP_ACCENT, Color.BLACK);
 
         ImageIcon healItemIcon = new ImageIcon("assets/images/items/heal.png");
-        int newWidth = 256;
-        int newHeight = 256;
+        int newWidth = 128;
+        int newHeight = 128;
         Image img = healItemIcon.getImage().getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
         ImageIcon healItemIconScaled = new ImageIcon(img);
         JLabel healItemIconLbl = new JLabel(healItemIconScaled);
@@ -129,28 +146,38 @@ public class ShopPanel extends Panel {
 
         healPanel.add(SizedBox.height(32));
         healPanel.add(healLbl);
-        healPanel.add(SizedBox.height(64));
+        healPanel.add(SizedBox.height(32));
         healPanel.add(healItemIconLbl);
-        healPanel.add(SizedBox.height(64));
+        healPanel.add(SizedBox.height(32));
 
         Row[] healItems = new Row[3];
         for (int i = 0; i < 3; i++) {
             healItems[i] = new Row();
             Label healItemLbl = new Label(
                     Javamon.getItem(i).getName() + " - " + Javamon.getItem(i).getPrice() + " gold",
-                    "jua", 16f, 8, Color.WHITE, Color.BLACK);
-            Button addItem = new Button("+", "Inter-Bold", 16f, Color.WHITE, Color.BLACK,
+                    "jua", 16f, 8, Colors.SHOP_ACCENT, Color.BLACK);
+            Button addItem = new Button("+", "Inter-Bold", 16f, Colors.SHOP_ACCENT, Color.BLACK,
                     addItem(itemToBuy, i, totalLbl));
-            Button removeItem = new Button("-", "Inter-Bold", 16f, Color.WHITE, Color.BLACK,
+            Button removeItem = new Button("-", "Inter-Bold", 16f, Colors.SHOP_ACCENT, Color.BLACK,
                     removeItem(itemToBuy, i, totalLbl));
 
-            healItems[i].add(Box.createHorizontalGlue());
+            addItem.setSize(48, 48);
+            addItem.setPreferredSize(new Dimension(48, 48));
+            addItem.setMaximumSize(new Dimension(48, 48));
+            addItem.setMinimumSize(new Dimension(48, 48));
+
+            removeItem.setSize(48, 48);
+            removeItem.setPreferredSize(new Dimension(48, 48));
+            removeItem.setMaximumSize(new Dimension(48, 48));
+            removeItem.setMinimumSize(new Dimension(48, 48));
+
+            healItems[i].add(SizedBox.width(8));
             healItems[i].add(healItemLbl);
             healItems[i].add(SizedBox.width(8));
             healItems[i].add(addItem);
             healItems[i].add(SizedBox.width(8));
             healItems[i].add(removeItem);
-            healItems[i].add(Box.createHorizontalGlue());
+            healItems[i].add(SizedBox.width(8));
 
             healPanel.add(healItems[i]);
         }
@@ -205,7 +232,7 @@ public class ShopPanel extends Panel {
     }
 
     private Button getBuyBtn(HomeGUI homeGUI, Map<Item, Integer> items) {
-        Button buyBtn = new Button("Buy", "jua", 16f, Color.WHITE, Color.BLACK, buyItems(homeGUI, items));
+        Button buyBtn = new Button("Buy", "jua", 16f, Colors.SHOP_ACCENT, Color.BLACK, buyItems(homeGUI, items));
         return buyBtn;
     }
 
@@ -215,7 +242,7 @@ public class ShopPanel extends Panel {
             public void actionPerformed(ActionEvent e) {
                 try {
                     Javamon.getHOMEBASE().buyItem(items);
-                    
+
                     homeGUI.setPanel("homebase");
                 } catch (GameException err) {
                     JOptionPane.showMessageDialog(homeGUI.getFrame(), err.getMessage(), "Error",
--- app/src/main/java/javamon/frontend/styles/Colors.java
+++ app/src/main/java/javamon/frontend/styles/Colors.java
@@ -10,6 +10,7 @@ public class Colors {
     public static final Color NEWGAME = new Color(76, 146, 87);
     public static final Color NEWGAME_ACCENT = new Color(149, 211, 158);
     public static final Color SHOP = new Color(30, 30, 30);
+    public static final Color SHOP_ACCENT = new Color(132, 234, 234);
     public static final Color HP = new Color(153, 108, 108);
     public static final Color Elemental = new Color(77, 63, 100);
     public static final Color LEVELUP = new Color(29, 109, 126);
